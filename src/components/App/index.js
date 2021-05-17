2670419cd741459dd4a86ab9386c45447fce1faf
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-05-17 16:39:47 +0200

    champs controlé

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Form from 'src/components/Form';
 import JokeButton from 'src/components/JokeButton';
@@ -8,21 +8,18 @@ import Joke from 'src/components/Joke';
 // == Composant
 const App = () => {
   const [dadName, setDadName] = useState('Alexandre');
+
   return (
-  <div>
-    <input
-        // value : j'insère ma donnée dans l'input HTML
-        value={dadName}
-        // onChange : lors d'une modification dde l'input, je sauvegarde la saisie
-        // dans mon state.
-        onChange={(event) => {
+    <div>
+      <Form
+        dadName={dadName}
+        onInputChange={(event) => {
           setDadName(event.target.value);
         }}
       />
-    <Form />
-    <JokeButton />
-    <Joke />
-  </div>
+      <JokeButton />
+      <Joke />
+    </div>
   );
 };
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './form.scss';
 
-const Form = () => (
-  <input className="input" type="text" placeholder="Nom de votre papa" />
+const Form = ({ dadName, onInputChange }) => (
+  <input
+    className="input"
+    type="text"
+    placeholder="Nom de votre papa"
+    value={dadName}
+    onChange={onInputChange}
+  />
 );
+
+Form.propTypes = {
+  dadName: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
 
 export default Form;
